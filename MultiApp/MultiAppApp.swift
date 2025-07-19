b30277c7cd8ab717562c5034641514d055fe5cb2
GeningAlexander <tuxid85@gmail.com>
2025-07-19 17:41:27 +0700

    Initial Commit

---
+++ MultiApp/MultiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiAppApp.swift
+//  MultiApp
+//
+//  Created by Alexandr Gening on 19.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiAppTests/MultiAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MultiAppTests.swift
+//  MultiAppTests
+//
+//  Created by Alexandr Gening on 19.07.2025.
+//
+
+import Testing
+
+struct MultiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
